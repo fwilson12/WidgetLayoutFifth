346d10105cf336e8b6d776c263e7e8a4b33420c3
s10043679 <fletcherbwilson1@gmail.com>
2023-09-21 10:33:17 -0500

    second commit 9/21

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 
 
@@ -12,6 +14,13 @@ class MainWidget(Widget):
 
 class WidgetLayoutApp(App):
     pass
+
+class StackLayoutExample(StackLayout):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = 'lr-tb'
+        cb = Button(text='Crazy Button',size_hint='.5,.5')
 
 WidgetLayoutApp().run()
 
